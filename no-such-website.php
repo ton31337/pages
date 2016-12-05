c08d01e88856877ccddda210aef3d5aabd54f0db
Andrius Putna <fordnox@gmail.com>
2016-12-05 18:14:57 +0200

    add no-such-website-page

---
+++ no-such-website.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset=utf-8>
+    <meta content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+          name=viewport>
+    <title>No such website | 000webhost </title>
+    <link rel="shortcut icon" type="image/x-icon" href="https://cdn.rawgit.com/000webhost/logo/master/favicon.ico">
+    <style>html, body {
+            font-family: sans-serif;
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+            height: 100%;
+            -webkit-font-smoothing: antialiased;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .message {
+            text-align: center;
+            align-self: center;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 0px 20px;
+            max-width: 450px;
+        }
+
+        .message_title {
+            font-size: 22px;
+            font-weight: 100;
+            margin-top: 15px;
+            color: #47494E;
+            margin-bottom: 8px;
+        }
+
+        p {
+            -webkit-margin-after: 0px;
+            -webkit-margin-before: 0px;
+            font-size: 15px;
+            color: #7F828B;
+            line-height: 21px;
+            margin-bottom: 4px;
+        }
+
+        .btn {
+            text-decoration: none;
+            padding: 8px 15px;
+            border-radius: 4px;
+            margin-top: 10px;
+            font-size: 14px;
+            color: #7F828B;
+            border: 1px solid #7F828B;
+        }
+
+        body.friendly {
+            background-color: #000;
+        }
+
+        body.friendly .message_title {
+            color: #fff;
+        }
+
+        body.friendly p {
+            color: rgba(255, 255, 255, 0.6);
+        }
+
+        body.friendly .btn {
+            color: #fff;
+            border: 1px solid rgba(255, 255, 255, 0.9);
+        }
+
+    </style>
+</head>
+
+<body class=friendly>
+<div class=spacer></div>
+<div class=message>
+    <img src="https://cdn.rawgit.com/000webhost/logo/master/000webhost-logo-forum-33x33.png" alt="free web hosting">
+    <div class=message_title> There's nothing here, yet.</div>
+    <a href="https://www.000webhost.com/free-website-sign-up?website=<?php echo $_SERVER['HTTP_HOST']?>&utm_source=no-such-website&utm_campaign=pages&utm_medium=no-such-website" class=btn>Start your website</a></div>
+</body>
+</html>
+
